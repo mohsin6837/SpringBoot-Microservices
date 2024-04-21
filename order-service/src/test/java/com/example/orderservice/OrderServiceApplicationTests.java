77e65b1a642205798a88a494067fa89fb5fb9a4c
Syed Mohsin <mohsinmehboob2@gmail.com>
2024-04-21 05:41:56 +0530

    Added files

--- order-service/src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/example/orderservice/OrderServiceApplicationTests.java
@@ -1,10 +1,8 @@
 package com.example.orderservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class OrderServiceApplicationTests {
+class OrderServiceApplicationTests extends AbstractIT{
 
 	@Test
 	void contextLoads() {
